34edfbee501b75d9ba9288f82866259abee4362b
datnguyen <nntd290897@gmail.com>
2020-09-22 00:23:17 +0700

    Updates

---
+++ precache-manifest.0f4a95cdd16787588eb93629b20d018e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f301d4916bf412fdebc7d080f0e440dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "863184b47b9409a070b6",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "513d3dc867d9a60642e2",
+    "url": "/static/css/main.9a41a157.chunk.css"
+  },
+  {
+    "revision": "863184b47b9409a070b6",
+    "url": "/static/js/2.476da85f.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.476da85f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513d3dc867d9a60642e2",
+    "url": "/static/js/main.bafd0c32.chunk.js"
+  },
+  {
+    "revision": "3aa8239169982fea0c6d",
+    "url": "/static/js/runtime-main.9e88b2ff.js"
+  }
+]);
